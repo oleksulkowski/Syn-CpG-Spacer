f9524430adaa201b47ee64d93a2f41d39e1d7eec
mroleksul <sulek2000@gmail.com>
2023-06-12 11:25:49 +0100

    Pages first commit

--- docs/app/app.js
+++ docs/app/app.js
@@ -1,3 +1,52 @@
+importScripts("https://cdn.jsdelivr.net/pyodide/v0.23.0/full/pyodide.js");
+
+function sendPatch(patch, buffers, msg_id) {
+  self.postMessage({
+    type: 'patch',
+    patch: patch,
+    buffers: buffers
+  })
+}
+
+async function startApplication() {
+  console.log("Loading pyodide!");
+  self.postMessage({type: 'status', msg: 'Loading pyodide'})
+  self.pyodide = await loadPyodide();
+  self.pyodide.globals.set("sendPatch", sendPatch);
+  console.log("Loaded!");
+  await self.pyodide.loadPackage("micropip");
+  const env_spec = ['markdown-it-py<3', 'https://cdn.holoviz.org/panel/1.1.0/dist/wheels/bokeh-3.1.1-py3-none-any.whl', 'https://cdn.holoviz.org/panel/1.1.0/dist/wheels/panel-1.1.0-py3-none-any.whl', 'pyodide-http==0.2.1', 'Bio', 'numpy', 'pandas']
+  for (const pkg of env_spec) {
+    let pkg_name;
+    if (pkg.endsWith('.whl')) {
+      pkg_name = pkg.split('/').slice(-1)[0].split('-')[0]
+    } else {
+      pkg_name = pkg
+    }
+    self.postMessage({type: 'status', msg: `Installing ${pkg_name}`})
+    try {
+      await self.pyodide.runPythonAsync(`
+        import micropip
+        await micropip.install('${pkg}');
+      `);
+    } catch(e) {
+      console.log(e)
+      self.postMessage({
+	type: 'status',
+	msg: `Error while installing ${pkg_name}`
+      });
+    }
+  }
+  console.log("Packages loaded!");
+  self.postMessage({type: 'status', msg: 'Executing code'})
+  const code = `
+  
+import asyncio
+
+from panel.io.pyodide import init_doc, write_doc
+
+init_doc()
+
 import pandas as pd
 import numpy as np
 from io import StringIO, BytesIO
@@ -355,7 +404,7 @@ class Gene:
 
         # Checks if the original amino acid sequence is different to the new one and prints an error if so. If original sequence has a stop codon change translated1 for translated1[:-3]
         if translated1 != translated2:
-            sys.exit('ERROR: Non-synonymous mutations were introduced!\n' + self.first_difference(translated1, translated2))
+            sys.exit('ERROR: Non-synonymous mutations were introduced!\\n' + self.first_difference(translated1, translated2))
     
     # Check if the packaging signals have been modified
     def check_packaging_signals(self):
@@ -709,15 +758,15 @@ def mutate(event, gap_method, option_value, protection_start_value, protection_e
     if gap_method == 1:
         mutation_settings = f"Minimum CpG gap set as {option_value}. "
     elif gap_method == 2:
-        mutation_settings = f"Desired average CpG gap set as {option_value}.\nAlgorithm-optimized minimal gap value is {gene.closest_gap}."
+        mutation_settings = f"Desired average CpG gap set as {option_value}.\\nAlgorithm-optimized minimal gap value is {gene.closest_gap}."
     if protection_start_value > 0 and protection_end_value == 0:
-        mutation_settings = mutation_settings + f"\nProtected {protection_start_value} initial nucleotides. "
+        mutation_settings = mutation_settings + f"\\nProtected {protection_start_value} initial nucleotides. "
     elif protection_start_value > 0 and protection_end_value > 0:
-        mutation_settings = mutation_settings + f"\nProtected {protection_start_value} initial and {protection_end_value} terminal nucleotides. "
+        mutation_settings = mutation_settings + f"\\nProtected {protection_start_value} initial and {protection_end_value} terminal nucleotides. "
     elif protection_start_value == 0 and protection_end_value > 0:
-        mutation_settings = mutation_settings + f"\nProtected {protection_start_value} terminal nucleotides. "
+        mutation_settings = mutation_settings + f"\\nProtected {protection_start_value} terminal nucleotides. "
     if A_rich:
-        mutation_settings = mutation_settings + f"\nA-enriched remaining codons. "
+        mutation_settings = mutation_settings + f"\\nA-enriched remaining codons. "
     
     CpG_change = gene.calculate_CpG_abundance_change()
     if CpG_change > 0:
@@ -824,3 +873,58 @@ successful_load_dummy.jscallback(value="""
 mutate_btn.param.watch(on_mutate_btn_click, 'clicks')
 
 template.servable()
+
+await write_doc()
+  `
+
+  try {
+    const [docs_json, render_items, root_ids] = await self.pyodide.runPythonAsync(code)
+    self.postMessage({
+      type: 'render',
+      docs_json: docs_json,
+      render_items: render_items,
+      root_ids: root_ids
+    })
+  } catch(e) {
+    const traceback = `${e}`
+    const tblines = traceback.split('\n')
+    self.postMessage({
+      type: 'status',
+      msg: tblines[tblines.length-2]
+    });
+    throw e
+  }
+}
+
+self.onmessage = async (event) => {
+  const msg = event.data
+  if (msg.type === 'rendered') {
+    self.pyodide.runPythonAsync(`
+    from panel.io.state import state
+    from panel.io.pyodide import _link_docs_worker
+
+    _link_docs_worker(state.curdoc, sendPatch, setter='js')
+    `)
+  } else if (msg.type === 'patch') {
+    self.pyodide.globals.set('patch', msg.patch)
+    self.pyodide.runPythonAsync(`
+    state.curdoc.apply_json_patch(patch.to_py(), setter='js')
+    `)
+    self.postMessage({type: 'idle'})
+  } else if (msg.type === 'location') {
+    self.pyodide.globals.set('location', msg.location)
+    self.pyodide.runPythonAsync(`
+    import json
+    from panel.io.state import state
+    from panel.util import edit_readonly
+    if state.location:
+        loc_data = json.loads(location)
+        with edit_readonly(state.location):
+            state.location.param.update({
+                k: v for k, v in loc_data.items() if k in state.location.param
+            })
+    `)
+  }
+}
+
+startApplication()
